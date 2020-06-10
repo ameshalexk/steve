79dc05881935588443f58482c7bb245c25decbc7
ameshalexk <ameshalexusa@gmail.com>
2020-06-09 23:17:03 -0400

    practised

---
+++ random.js
@@ -0,0 +1,11 @@
+// const user = {name: "Amesh"};
+
+// localStorage.setItem("usercopy", JSON.stringify(user));
+
+// localStorage.getItem(usercopy);
+"use strict";
+
+
+let c = 55;
+
+console.log(c);
