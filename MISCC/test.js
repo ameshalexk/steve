1c1389b418fee06f83c34ad3d71db6c43ce68aee
ameshalex <ameshalexusa@gmail.com>
2020-06-29 00:52:33 -0400

    sds

--- MISCC/test.js
+++ MISCC/test.js
@@ -1,4 +1,6 @@
-console.log("sds");
-asd
-asdsad
-asdsad132d
+const memo = {};
+    const names = ['Alex', 'Charlie', 'Dolores', 'May', 'Alex', 'Claire', 'Dolores', 'Claire',  'Alex']
+    for(let i = 0; i < names.length; i++){
+        let name = names[i];
+        memo[name] = i;
+    }
